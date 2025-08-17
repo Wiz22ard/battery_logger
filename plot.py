e0366fb0744725592355c56e1b26a54bc6e1fcf5
Wiz22ard <learningcentre50@gmail.com>
2025-08-18 00:00:47 +0700

    Update CSV - 2025-08-18T00:00:47.930842

--- plot.py
+++ plot.py
@@ -3,28 +3,56 @@ import matplotlib.pyplot as plt
 import subprocess as sp
 from datetime import datetime
 import os
+import glob
 
-files = ['voltage', 'temperature', 'percentage']
+categories = ['voltage', 'temperature', 'percentage']
+cat_path = "/data/data/com.termux/files/home/storage/termux/api/battery"
 
-for file in files:
-    date_str = datetime.now().strftime('%Y-%m-%d')
-    csv_file = os.path.join(file, date_str) + '.csv'
-    df = pd.read_csv(csv_file)
+n_plots = len(categories)
+
+fig, axs = plt.subplots(n_plots, 1, 
+                        figsize = (20,5 * n_plots), 
+                        dpi = 300, sharex = True)
+
+fig.suptitle("Daily Battery Logger Plots",
+             fontsize = 30,
+             fontweight = 'bold',
+             fontstyle = 'italic',
+             y = .99)
+
+for i, category in enumerate(categories):
+    csv_files = glob.glob(os.path.join(cat_path, category, '*.csv'))
+    
+    df = pd.DataFrame()
+    for csv_file in csv_files[1:]:
+        df_temp = pd.read_csv(csv_file)
+        df = pd.concat([df, df_temp], ignore_index = True)
+
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df.sort_values(by='timestamp', inplace=True)
 
-    fig = plt.figure(figsize=(20,5), dpi=300)
     for val in df['status'].unique():
         subset = df.loc[df['status']==val]
-        plt.plot(subset['timestamp'], subset['value'], label=val)
+        axs[i].scatter(subset['timestamp'], subset['value'], 
+                       label=val, s = 5)
     
-    plt.title(f"{file.upper()} Daily Plot", 
-              {'fontsize': 36, 'fontweight':'bold'}, 
-              pad=12)
-    plt.ylabel(file)
-    plt.xlim(df['timestamp'].min(), df['timestamp'].max())
-    plt.ylim(df['value'].min(), df['value'].max())
-    plt.grid(True)
-    plt.legend()
-    plt.savefig(f"/data/data/com.termux/files/home/storage/downloads/{file}.png")
-    print(csv_file)
+    #axs[i].set_title(f'- {category.capitalize()}',
+    #                 loc = 'left',
+    #                 fontweight = 'bold')
+
+    axs[i].set_ylabel(category.capitalize(), fontweight = 'bold')
+    axs[i].set_xlim(df['timestamp'].min(), df['timestamp'].max())
+    axs[i].set_ylim(df['value'].min(), df['value'].max())
+    axs[i].grid(True)
+
+plt.legend(loc = 'upper left', 
+           bbox_to_anchor = (0,-.2,1,.102), 
+           borderaxespad=0,
+           ncols = len(df['status'].unique()), 
+           mode = 'expand',
+           frameon = False)
+plt.tight_layout()
+
+download_path = "/data/data/com.termux/files/home/storage/downloads"
+plt.savefig(download_path + "/daily.png")
+print("Daily Plot has been saved! in", download_path)
